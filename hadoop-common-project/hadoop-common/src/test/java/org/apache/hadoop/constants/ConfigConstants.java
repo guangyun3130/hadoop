1d20cf146d563fa28a8d89d74dfb2d792c4d55a1
Dominik Diedrich <dominik.diedrich@ibm.com>
2024-11-07 11:39:22 +0100

    Fixed more style checks

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/constants/ConfigConstants.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/constants/ConfigConstants.java
@@ -18,12 +18,15 @@
 
 package org.apache.hadoop.constants;
 
-public interface ConfigConstants {
+public final class ConfigConstants {
+
+  private ConfigConstants() {}
+
  /**
   * System property name for the avro dependency.
   * This property is used to configure trusted packages,
   * which the avro dependency can use for serialization.
   */
-  static final String CONFIG_AVRO_SERIALIZABLE_PACKAGES =
-    "org.apache.avro.SERIALIZABLE_PACKAGES";
+  public static final String CONFIG_AVRO_SERIALIZABLE_PACKAGES =
+      "org.apache.avro.SERIALIZABLE_PACKAGES";
 }
